6e5598ea9f83bdebc96495ffe0aed2993b84ce75
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-05-03 11:32:01 -0500

    menu responsive(falta js)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+// var btnmenu = document.getElementsByClassName('btn-menu');
+// var menuBar = document.getElementById('menu');
+// var contador = 1;
+// function menu (){
+//   if(contador== 1){
+//     menuBar.getAttribute('left','100%');
+//   } else {
+//
+//   }
+// };
